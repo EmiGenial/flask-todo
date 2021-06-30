c07ce5877b5643204b58bbbd861162b3f08ed9fc
EmiGenial <developer@gmail.com>
2021-06-30 19:44:32 -0300

    3) Herencia de Templates

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -14,6 +16,10 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip':user_ip,
+        'todos':todos
+    }
 
-    return render_template('hello.html', user_ip=user_ip)
+    return render_template('hello.html', **context)
 
